28028b0799418d92a11e65006a3a30f93d50ef71
Anirudh Dinesh <anirudhdinesh@Anirudhs-MacBook-Pro.local>
2021-01-25 17:17:14 -0500

    course-listing

---
+++ js/courses.js
@@ -0,0 +1,60 @@
+////////////////////////////////////////
+// reload page after Forward and back
+///////////////////////////////////////
+
+const TYPE_BACK_FORWARD = 2;
+
+function isReloadedPage() {
+  return performance.navigation.type === TYPE_BACK_FORWARD;
+}
+
+function main() {
+  if (isReloadedPage()) {
+    window.location.reload();
+  }
+}
+main();
+
+////////////////////////////////////////////////////////////
+///// TEAM  API REQUEST ` `
+////////////////////////////////////////////////////////////
+
+
+Vue.use(VueMeta);
+new Vue({
+
+  el: '#site-page',
+
+  data () {
+    return {
+      sitesData: [],
+      apiURL: 'https://directus.thegovlab.com/thegovlab/items/sites'
+    }
+  },
+  created: function created() {
+    this.fetchSites();
+  },
+  methods: {
+
+    fetchSites() {
+      self = this;
+      const client = new DirectusSDK({
+        url: "https://directus.thegovlab.com/",
+        project: "thegovlab",
+        storage: window.localStorage
+      });
+
+      client.getItems(
+  'sites',
+  {
+    fields: ['*.*','thumbnail.*']
+  }
+).then(data => {
+  self.indexData = data.data;
+  let tempData = self.indexData.filter(items => (items.type.includes("course")));
+  self.sitesData = tempData;
+})
+.catch(error => console.error(error));
+    }
+  }
+});
