c5d278d5767c4763d38aa758b99948a3047c9a91
Anirudh Dinesh <anirudhdinesh@Anirudhs-MacBook-Pro.local>
2021-01-28 16:24:11 -0500

    blog

---
+++ js/blog.js
@@ -0,0 +1,62 @@
+////////////////////////////////////////
+// reload page after Forward and back
+///////////////////////////////////////
+
+const TYPE_BACK_FORWARD = 2;
+
+function isReloadedPage() {
+  return performance.navigation.type === TYPE_BACK_FORWARD;
+}
+
+function main() {
+  if (isReloadedPage()) {
+    window.location.reload();
+  }
+}
+main();
+
+////////////////////////////////////////////////////////////
+///// TEAM  API REQUEST ` `
+////////////////////////////////////////////////////////////
+
+
+Vue.use(VueMeta);
+new Vue({
+
+  el: '#site-page',
+
+  data () {
+    return {
+      blogData: [],
+      apiURL: 'https://directus.thegovlab.com'
+    }
+  },
+  created: function created() {
+    this.fetchPosts();
+  },
+  methods: {
+
+    fetchPosts() {
+      self = this;
+      const client = new DirectusSDK({
+        url: "https://directus.thegovlab.com/",
+        project: "thegovlab",
+        storage: window.localStorage
+      });
+
+      client.getItems(
+  'blog',
+  {
+    sort:"-created_on",
+    fields: ['*.*']
+  }
+).then(data => {
+  self.fullData = data.data;
+  let tempData = self.fullData.filter(items => (items.categories.includes('cat_38')));
+self.blogData = tempData;
+})
+.catch(error => console.error(error));
+    }
+  }
+});
+
