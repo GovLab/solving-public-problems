22b2f3f6a647a50236a8941286ba71459da5ec5a
Stephan <schmid91@mail.com>
2022-06-17 15:16:59 -0400

    changes twitter feed2

--- src/main.js
+++ src/main.js
@@ -3,11 +3,10 @@ import './assets/styles.css'
 import App from './App.vue'
 import router from './router'
 import VueGtag from "vue-gtag"
-import Tweet from 'vue-tweet'
 
 createApp(App).use(
     router,
     VueGtag, {
   config: { id: "G-LS299J2EWH" },
   enabled: true, 
-}, Tweet).mount('#app')
+}).mount('#app')
