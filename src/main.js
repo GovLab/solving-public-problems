a0af71766d5be913092c11670f6ae8eddf591275
Stephan <schmid91@mail.com>
2023-12-18 18:35:45 -0800

    try this

--- src/main.js
+++ src/main.js
@@ -2,11 +2,9 @@ import { createApp } from 'vue'
 import './assets/styles.css'
 import App from './App.vue'
 import router from './router'
-import VueGtag from "vue-gtag"
 
 createApp(App).use(
     router,
-    VueGtag, {
-  config: { id: "G-LS299J2EWH" },
-  enabled: true, 
-}).mount('#app')
+).mount('#app')
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,70 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import ViteFonts from 'vite-plugin-fonts'
+// import Pages from "vite-plugin-pages";
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/",
+//   plugins: [
+//     vue(),
+//     Pages({
+//       dirs: 'src/pages',
+//       extensions: ['vue', 'ts'],
+//     }),
+//     ViteFonts({
+//       typekit: {
+//           /**
+//            * Typekit project id
+//            */
+//           id: 'fnf8omo',
+
+//           /**
+//            * enable non-blocking renderer
+//            *   <link rel="preload" href="xxx" as="style" onload="this.rel='stylesheet'">
+//            * default: true
+//            */
+//           defer: true
+//         },
+//     })
+//   ],
+//   server: {
+//     host: '0.0.0.0',
+//     hmr: {
+//       host: 'localhost', // you could make this an ENV var
+//       port: '3005',
+//       path: '/'
+//     }
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePluginRadar } from "vite-plugin-radar";
 import ViteFonts from 'vite-plugin-fonts'
+import { format } from 'date-fns';
+import { isPast } from 'date-fns';
+
 import Pages from "vite-plugin-pages";
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
+  assetsInclude: ['**/*.png'],
+  data () {
+    return {
+      format,
+      isPast
+    }
+  },
   plugins: [
+    VitePluginRadar({
+      // Google Analytics tag inject
+      enableDev: true,
+      analytics: {
+        id: 'G-LS299J2EWH',
+      },
+    }),
     vue(),
     Pages({
       dirs: 'src/pages',
@@ -35,4 +94,8 @@ export default defineConfig({
       path: '/'
     }
   },
-})
+}
+
+)
+
+
